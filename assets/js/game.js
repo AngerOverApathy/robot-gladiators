d8d5f0af2c9864d4aa92e103ac69fb2c8ce5c474
AngerOverApathy <angersavestheworld@gmail.com>
2022-01-03 17:33:24 -0800

    added console commands

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName=window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This log is a string, good for leaving messages!")
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+//fight();
