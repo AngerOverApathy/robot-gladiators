775de57c823b1e0dee95660807c9acd4e71b7054
AngerOverApathy <violasian@gmail.com>
2022-07-23 15:39:25 -0700

    js adjusted

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-
+var playerMoney = 10;
 
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
